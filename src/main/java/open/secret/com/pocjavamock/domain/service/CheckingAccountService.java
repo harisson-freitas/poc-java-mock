fd007cee12fee17607c2a1aa156c6e052cbf40cd
Harisson Freitas [ Zallpy ] <harry.freitas@hotmail.com>
2022-03-15 09:06:58 -0300

    feat: Update CheckingAccountService

--- src/main/java/open/secret/com/pocjavamock/domain/service/CheckingAccountService.java
+++ src/main/java/open/secret/com/pocjavamock/domain/service/CheckingAccountService.java
@@ -3,6 +3,7 @@ package open.secret.com.pocjavamock.domain.service;
 import org.springframework.stereotype.Service;
 
 import open.secret.com.pocjavamock.domain.CheckingAccount;
+import open.secret.com.pocjavamock.domain.User;
 
 @Service
 public class CheckingAccountService {
@@ -16,6 +17,14 @@ public class CheckingAccountService {
     public CheckingAccount getCheckingAccount(final Integer number) {
         var user = userService.getUser();
         
+        validation(user);
+
         return new CheckingAccount(1L, number, 10.000, 500.000, user);
+    }
+
+    private void validation(User user) {
+        if (user.address() != null) {
+            System.out.println("TESTE");
+        }
     }
 }
